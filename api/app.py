80f98dcc457eee8482c92ed0bcbaff39ba5662e3
Jakub Raban <jakub.raban@gmail.com>
2023-06-30 18:30:35 +0200

    Configure Webpack, update react to 18

--- api/app.py
+++ api/app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='dist', static_folder='dist')
 app.debug = True
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 import views.viewone
 import views.viewtwo
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
 
 if __name__ == '__main__':
     socketio.run(app)
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {StrictMode} from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root') as HTMLElement
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

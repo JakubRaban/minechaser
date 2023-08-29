8df1a02e547899eeb78e6ff0a4adcf5c49934bfc
Jakub Raban <jakub.raban@gmail.com>
2023-08-29 20:42:38 +0200

    Main menu styling

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
-import { NameSetter } from './NameSetter/NameSetter'
 import { useLocation } from 'react-router'
 import { Button } from '../lib/Button/Button'
 
@@ -13,23 +12,24 @@ export const LandingPage: FC = () => {
         <div className="landing-page">
             <header>
                 <h1 className="title">MineChaser</h1>
+                <h6 className="subtitle">Where Minesweeper meets Battle Royale</h6>
             </header>
-            <main className="grid">
-                <div className="menu">
+            <main>
+                <section className="menu">
                     <Button text="Play Online" link="/queue" />
                     <Button text="Play With Friends" link="/new-game" />
                     <Button text="Single Player" link="/new-game/single-player" />
-                </div>
-                <div className="rules">
+                </section>
+                <section className="rules">
                     <h2>Quick Rules</h2>
                     <ul>
                         <li>
-                            Basic mechanics of this game are the same as in the traditional Minesweeper - for a quick overview you can visit&nbsp;
-                            <a href="https://minesweepergame.com/strategy/how-to-play-minesweeper.php">this page</a>
+                            Basic mechanics of this game are the same as in the&nbsp;
+                            <a href="https://minesweepergame.com/strategy/how-to-play-minesweeper.php">traditional Minesweeper</a>
                         </li>
                         <li>
-                            Instead of clicking on the board, players move around it using the arrow keys.
-                            When you step on a cell, it&apos;s as if you left-clicked it in a Minesweeper game.
+                            Instead of clicking on the board, move around it using the arrow keys.
+                            Stepping on a cell is like left-clicking it in Minesweeper.
                             If you step on a mine, you&apos;re eliminated!
                         </li>
                         <li>
@@ -37,11 +37,11 @@ export const LandingPage: FC = () => {
                             You also get penalty points for flagging a safe cell.
                         </li>
                         <li>
-                            Stay alive and gain more points than your opponents to win.
+                            Stay alive and gain more points than your opponents to win!
                         </li>
                     </ul>
-                    <div>You can find detailed rules <Link to="/how-to-play">here</Link></div>
-                </div>
+                    <div>You can find more detailed rules <Link to="/how-to-play">here</Link></div>
+                </section>
             </main>
             <footer>
                 (C) Jakub Raban 2023. All rights reserved.

968dd50b40b6e6cd8526cded243c95075bd5eedc
Jakub Raban <jakub.raban@gmail.com>
2023-10-30 20:36:06 +0100

    Fix admin console

--- api/views/admin.py
+++ api/views/admin.py
@@ -1,4 +1,6 @@
 import os
+import sys
+from io import StringIO
 
 from socketsetup import sio, socket_id_to_player_id, player_id_to_player_name
 from services.game import GameService
@@ -14,12 +16,14 @@ password = os.environ.get('GOD_TOKEN')
 @sio.event(namespace='/god')
 def command(_, data: dict):
     sent_password = data.pop('password', None)
-    sent_command = data.pop('command', None)
-    is_eval = data.pop('iseval', False)
-    print(sent_command)
-    print(sent_password == password)
+    sent_command = data.pop('evalcommand', None) or data.pop('execcommand', None)
+    is_eval = data.pop('iseval', 'off')
     if password and sent_command and sent_password == password:
-        if is_eval:
+        if is_eval == 'on':
             return eval(sent_command)
-        exec(sent_command)
-        return 'OK'
+        else:
+            old_stdout = sys.stdout
+            sys.stdout = mystdout = StringIO()
+            exec(sent_command)
+            sys.stdout = old_stdout
+            return mystdout.getvalue() or 'OK'

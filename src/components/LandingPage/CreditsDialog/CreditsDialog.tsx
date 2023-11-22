503d97fe6adde47466b3b9abbbf318ab0a4aae37
Jakub Raban <jakub.raban@gmail.com>
2023-11-22 22:17:34 +0100

    Add credits

--- src/components/LandingPage/CreditsDialog/CreditsDialog.tsx
+++ src/components/LandingPage/CreditsDialog/CreditsDialog.tsx
@@ -5,6 +5,7 @@ const CreditsDialog = () => (
         <DialogHeader title="Credits" />
         <div>
             <p>Music by Eric Matyas &ndash; <a href="http://soundimage.org" target="_blank" rel="noreferrer">soundimage.org</a></p>
+            <p><a href="https://www.vecteezy.com/free-vector/snowflake-icon">Snowflake Icon Vectors by Vecteezy</a></p>
         </div>
     </Dialog>
 )
